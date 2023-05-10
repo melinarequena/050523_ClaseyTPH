f11fe286879915e28743921422451378c6e8046b
Melina <mrequena@fue.undef.edu.ar>
2023-05-10 18:25:30 -0300

    Planteo

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
+//Se cuenta con un depósito (nombre) que recibe paquetes (nodos) y luego los entrega.
+//Los paquetes al ingresar sobre en la puerta A, se los lleva lo más cercano posible a la puerta de entrega B.
+//Para retirar, siempre se entrega el paquete más próximo a la puerta B.
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
-}
+//TPH: Relizar los comportamientos para: 1. ingresarPrioridad (lo deja en la puerta B) 2. entregaEspecial (retirar un
+//paquete con un código específico)
+
